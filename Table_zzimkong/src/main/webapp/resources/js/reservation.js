5082e53cd94234426953a7cbd3ca3a28237de7c1
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-12-12 16:35:23 +0900

    이재환 - 자바스크립트 추가

--- Table_zzimkong/src/main/webapp/resources/js/reservation.js
+++ Table_zzimkong/src/main/webapp/resources/js/reservation.js
@@ -1,3 +1,4 @@
+//가게유의사항 전체체크
 function checkbox_allCheck(){
 	for(let item of document.reservation.check){
 		item.checked = document.reservation.checkAll.checked;
@@ -5,22 +6,45 @@ function checkbox_allCheck(){
 }
 
 
-function submit() {
-	if(document.reservation.name.value == ""){
-		alert("예약자 성함 입력 필수!")
-		document.reservation.name.focus();
-		return false;
-			
-	}else if(document.reservation.number.value == ""){
-		alert("예약자 연락처 입력 필수!")
-		document.reservation.number.focus();
-		return false;
 
-	}else if(document.reservation.agree.value == ""){
-		alert("예약약관에 동의해주세요.")
-		document.reservation.agree.focus();
-		return false;
-	}
+//성명, 연락처, 약관동의 값없으면 포커스 이동
+function btnPay() {
+  var resName = document.forms["reservation"]["resName"];
+  var resNumber = document.forms["reservation"]["resNumber"];
+  var checkbox = document.getElementById("checkbox_agree");
+
+  if (resName.value == "") {
+    alert("성명을 입력해주세요.");
+    resName.focus();
+    return false;
+  }
+
+  if (resNumber.value == "") {
+    alert("연락처를 입력해주세요.");
+    resNumber.focus();
+    return false;
+  }
+  
+  if (!checkbox.checked) {
+    alert("예약 약관에 동의해주세요.");
+    checkbox.focus();
+    return false;
+  }
 				
 	return true;
+}
+
+// 예약자정보 방문자정보에 복사 입력
+window.onload = function() {
+  document.getElementById("checkbox_same_info").addEventListener("click", copyInfo);
+}
+
+function copyInfo() {
+  if (document.getElementById("checkbox_same_info").checked) {
+    document.getElementsByName("name")[0].value = document.getElementsByName("resName")[0].value;
+    document.getElementsByName("number")[0].value = document.getElementsByName("resNumber")[0].value;
+  } else {
+    document.getElementsByName("name")[0].value = "";
+    document.getElementsByName("number")[0].value = "";
+  }
 }
