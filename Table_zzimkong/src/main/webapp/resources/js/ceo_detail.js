10cd39dd51e217d6ad44f0e5f546c086e9e2126c
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-12-13 16:56:10 +0900

    이재환 - 12/13 예약페이지, 업주 예약관리 페이지 수정

---
+++ Table_zzimkong/src/main/webapp/resources/js/ceo_detail.js
@@ -0,0 +1,3 @@
+function newDetails(){
+	window.open("reservation/detail", "address_window", "width = 700, height = 800");
+}
--- Table_zzimkong/src/main/webapp/resources/js/ceo_detaile.js
+++
@@ -1,3 +0,0 @@
-function newDetails(){
-	window.open("com_detaile.jsp", "address_window", "width = 700, height = 800");
-}
