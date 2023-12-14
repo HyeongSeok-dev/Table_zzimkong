440b54c6a0f60cee6582f85be399540d3724f952
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-12-14 11:20:32 +0900

    이형석 12/14 검색바 수정

--- Table_zzimkong/src/main/webapp/resources/js/search_bar.js
+++ Table_zzimkong/src/main/webapp/resources/js/search_bar.js
@@ -2,14 +2,22 @@ $(document).ready(function() {
 	// 모든 모달창을 숨깁니다.
 	$('.modal').hide();
 
-	// 모달창을 열고 닫는 코드
+	function closeAllModals() {
+		$('.modal').hide();
+	}
+
+	// 필터 모달 오프너 클릭 이벤트
 	$('.filter_wrapper').click(function() {
-		$('#filterModal').show();
+		closeAllModals(); // 다른 모달 닫기
+		$('#filterModal').show(); // 필터 모달 열기
 	});
 
+	// 테이블 모달 오프너 클릭 이벤트
 	$('.search_info').first().click(function() {
-		$('#table_modal').show();
+		closeAllModals(); // 다른 모달 닫기
+		$('#table_modal').show(); // 테이블 모달 열기
 	});
+
 	// 닫기 버튼을 클릭했을 때, 해당 버튼이 속한 모달창만을 닫습니다.
 	$(document).on('click', '.close', function() {
 		$(this).closest('.modal').hide();
@@ -21,8 +29,7 @@ $(document).ready(function() {
 	// 모달창 바깥 부분을 클릭했을 때, 해당 부분이 속한 모달창만을 닫습니다.
 	$(window).click(function(event) {
 		if (!$(event.target).closest('.modal').length && !$(event.target).hasClass('modal-opener')) {
-			$('.modal').hide();
-			console.log("모달창 밖 클릭됨")
+			closeAllModals();
 		}
 	});
 
@@ -127,39 +134,62 @@ $(document).ready(function() {
 
 	$('.sub-list, .detail-list').css('display', 'none');
 
-	// 리스트 항목 클릭 시 라디오 버튼 변경 (이벤트 위임 사용)
-	$(document).on('click', '.sub-list li, .detail-list li', function(e) {
+	// '.filter-item' 클릭 시 이벤트 핸들러
+	$('.filter-item').click(function() {
+		var subList = $(this).find('.sub-list').clone(true); // 해당하는 서브 리스트를 복제합니다.
+		$('.sub-list-wrap').empty().append(subList); // 서브 리스트 래퍼에 복제된 서브 리스트를 추가합니다.
+
+		// 디테일 리스트를 비웁니다.
+		$('.detail-list-wrap').empty();
+		subList.css('display', 'flex'); // 서브 리스트를 표시합니다.
+	});
+
+	$('.sub-list > li').click(function(event) {
+		// 현재 클릭된 요소에 대한 상세 리스트가 있는지 확인합니다.
+		event.stopPropagation();
+		
+		var detailList = $(this).find('.detail-list');
+			// 상세 리스트가 있는 경우, 복제하고 '.detail-list-wrap'에 추가합니다.
+			var clonedDetailList = detailList.clone(true);
+			$('.detail-list-wrap').empty().append(clonedDetailList);
+			clonedDetailList.css('display', 'flex');
+			console.log("리스트 만들어짐")
+	});
+
+	$(document).on('click', '.filter-list li, .sub-list li, .detail-list li', function(e) {
 		var radio = $(this).find('input[type="radio"]');
 		var wasChecked = radio.prop('checked');
 
-		if (!wasChecked) { // 이미 선택된 라디오 버튼을 다시 클릭한 경우는 제외
-			radio.prop('checked', true); // 상태를 변경
+		if (!wasChecked) {
+			// 현재 클릭된 항목에 따라 다른 레벨의 checked 클래스를 제거
+			if ($(this).parents('.filter-list').length) {
+				// filter-list 클릭 시
+				$('.filter-list li').removeClass('selected');
+				$('.sub-list li').removeClass('selected');
+				$('.detail-list li').removeClass('selected');
+			console.log("필터리스트 클릭됨")
+				
+			} else if ($(this).parents('.sub-list').length) {
+				// sub-list 클릭 시
+				$(this).closest('.sub-list').find('li').removeClass('selected');
+				$('.detail-list li').removeClass('selected');
+				console.log("서브리스트 클릭됨")
+			} else if ($(this).parents('.detail-list').length) {
+				// detail-list 클릭 시
+				$(this).closest('.detail-list').find('li').removeClass('selected');
+			console.log("디테일 리스트 클릭됨")
+				
+			}
 
-			// 클래스 변경을 강제로 적용
-			$('.filter-list li, .sub-list li, .detail-list li').removeClass('selected');
+			// 선택된 라디오 버튼에 대한 클래스 적용
+			radio.prop('checked', true);
 			$(this).addClass('selected');
-
 		}
 	});
 
 	$('.sub-list').on('input', '#price', function() {
 		$('#priceValue').text($(this).val());
 		console.log("슬라이더 값 변경: ", $(this).val());
-	});
-
-	$('.filter-item > span').click(function() {
-		var subList = $(this).siblings('.sub-list').clone(true);
-		$('.sub-list-wrap').empty().append(subList);
-
-		// 디테일 리스트를 비웁니다.
-		$('.detail-list-wrap').empty();
-		subList.css('display', 'flex');
-	});
-
-	$('.sub-list > li').click(function() {
-		var detailList = $(this).find('.detail-list').clone(true);
-		$('.detail-list-wrap').empty().append(detailList);
-		detailList.css('display', 'flex');
 	});
 
 });
