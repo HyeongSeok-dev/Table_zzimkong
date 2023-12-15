b3a57e833f3f874ce5f2a68156d1ea7cee39e080
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-12-15 15:04:12 +0900

    이형석 12-15

---
+++ Table_zzimkong/src/main/java/com/table/zzimkong/vo/SearchVO.java
@@ -0,0 +1,18 @@
+package com.table.zzimkong.vo;
+
+import lombok.Data;
+
+@Data
+public class SearchVO {
+	String context ;
+	int persons;
+	String time;
+	String date;
+	String location;
+	String category;
+	String price;
+	String mood;
+	String facilities;
+	String table_type;
+	String hygiene;
+}
