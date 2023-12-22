bc3ccce3a0f1bdedf8a2725233bdd7c314fbc235
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-12-22 17:21:34 +0900

    docs: 페이지 일부 수정 (고은) - 12/22

--- Table_zzimkong/src/main/webapp/resources/js/login.js
+++ Table_zzimkong/src/main/webapp/resources/js/login.js
@@ -8,18 +8,9 @@ function popupFindId(){
 		    window.open("login/find/id", "address_window", "width=500,height=550");
 		}
 		
-		document.getElementById('find_id').addEventListener('click', function(event) {
-		    event.preventDefault();
-		    popupFindId();
-		});
 		  
 //비밀번호 누르면 팝업창(login_findPasswd.jsp)이동
 function popupFindPasswd(){
 		    window.open("login/find/passwd", "address_window", "width=500,height=550");
 		}
 		
-		
-		document.getElementById('find_passwd').addEventListener('click', function(event) {
-		    event.preventDefault();
-		    popupFindPasswd();
-		});
