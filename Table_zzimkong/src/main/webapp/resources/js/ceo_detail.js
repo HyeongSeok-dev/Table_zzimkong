d584e17c772e70f363016c9a593066c21808439e
HP <HP@leejaehwawn>
2023-12-26 00:27:54 +0900

    test : HomeController ceo_reservation_info 테스트 매핑 feat : 예약관리페이지 테이블 수정 예약상세정보 팝업창 생성 (이재환) - 12/26

--- Table_zzimkong/src/main/webapp/resources/js/ceo_detail.js
+++ Table_zzimkong/src/main/webapp/resources/js/ceo_detail.js
@@ -1,7 +1,17 @@
+//예약현황정보
 function newDetails(){
 	var popupW = 850;
 	var popupH = 600;
 	var left = Math.ceil((window.screen.width - popupW)/2);
 	var top = Math.ceil((window.screen.height - popupH)/2);
 	window.open("reservation/detail", "address_window", 'width='+popupW+',height='+popupH+',left='+left+',top='+top+',scrollbars=yes,resizable=no,toolbar=no,titlebar=no,menubar=no,location=no')
+}
+
+//예약정보
+function newInfo() {
+  var url = "reservation/info";
+  var name = "New Info";
+  var specs = "width=1200,height=600";  // 팝업창의 너비와 높이를 설정합니다. 필요에 따라 조정하세요.
+  
+  window.open(url, name, specs);
 }
