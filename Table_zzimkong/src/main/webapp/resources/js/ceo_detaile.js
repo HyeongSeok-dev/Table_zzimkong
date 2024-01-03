0baa333a3214a9950fbc8ac4f1ae20a00aa08997
HP <HP@leejaehwawn>
2024-01-03 15:58:28 +0900

    미사용파일 삭제(이재환) - 1/3

--- Table_zzimkong/src/main/webapp/resources/js/ceo_detaile.js
+++
@@ -1,3 +0,0 @@
-function newDetails(){
-	window.open("reservation/detail", "address_window", 'width='+popupW+',height='+popupH+',left='+left+',top='+top+',scrollbars=yes,resizable=no,toolbar=no,titlebar=no,menubar=no,location=no');
-}
